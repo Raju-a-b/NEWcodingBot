fa1a54a401167b98fa361674594932269f631183
NEWcodingBot <muggariraju@gmail>
2024-07-18 16:22:57 +0530

    feature202 chnage by Application.java

---
+++ Application.java
@@ -0,0 +1,6 @@
+#feature202 change by raju module
+      if ( args == null || args.length != 4 )
+      {
+        System.out.println(
+          "Usage:\n  CreateWebSource <webServiceURL> <userName> <password> <webSourceURL>"
+      #end feature202
