6882afdba559a3d11e63f26dcfb39a580482235a
NEWcodingBot <muggariraju@gmail>
2024-07-18 16:16:28 +0530

    feature 201 changes Appilication.java

---
+++ Application.java
@@ -0,0 +1,8 @@
+#feature201 change by raju payment module
+import java.util.List;
+import java.net.URL;
+ 
+import javax.xml.ws.BindingProvider;
+import javax.xml.namespace.QName;
+ #end feature201
+ 
